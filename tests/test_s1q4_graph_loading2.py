61e0eb3e000a622be155164ba14854e3e5cb42a3
Tad73 <127527810+Tad73@users.noreply.github.com>
2023-03-16 09:14:36 +0100

    Update test_s1q4_graph_loading2.py

--- tests/test_s1q4_graph_loading2.py
+++ tests/test_s1q4_graph_loading2.py
@@ -1,1 +1,14 @@
-# À compléter
+import sys
+sys.path.append("delivery_network/")
+
+import unittest
+from graph import graph_from_file
+
+class Test_GraphLoading2(unittest.TestCase):
+    def test_network5(self):
+        g = graph_from_file("input/network.05.in")
+        self.assertEqual(g.nb_nodes, 4)
+        self.assertEqual(g.nb_edges, 6)
+
+if __name__ == '__main__':
+    unittest.main()
