4674bca283e3eb3dceafd8e2e238f39f9aa1ae6c
Tad73 <127527810+Tad73@users.noreply.github.com>
2023-03-16 09:06:27 +0100

    Update test_s1q5_min_distance.py

--- tests/test_s1q5_min_distance.py
+++ tests/test_s1q5_min_distance.py
@@ -1,1 +1,14 @@
-# À compléter
+import sys
+sys.path.append("delivery_network/")
+
+import unittest
+from graph import Graph, graph_from_file
+
+class test_Graph(unittest.TestCase):
+    def test_network4(self):
+        g = graph_from_file("input/network.04.in")
+        self.assertEqual(g.get_path_with_power(1, 4, 11), [1, 4])
+        self.assertEqual(g.get_path_with_power(1, 4, 7), [1, 2, 3, 4])
+
+if __name__ == '__main__':
+    unittest.main()
